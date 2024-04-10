1393d0616f506d6a3ca3f657d2436f0957f76368
eduuramirooo <88739436+eduuramirooo@users.noreply.github.com>
2024-04-10 10:38:59 +0200

    commit hecho xd

---
+++ nuevoArcade/js/funciones.js
@@ -0,0 +1,44 @@
+ function fl() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+window.onload = () => {
+fl();
+
+      
+    let indice = 0;
+    let etiquetaImg;
+    const informacion = 
+        [
+            {
+                nombre: "Tekken",
+                imagen: "Tekken.jpg",
+                audio: "tekken.mp3"
+            },
+            {
+                nombre: "Metal Slug",
+                imagen: "Metal.jpg",
+                audio: "metal.mp3"
+            },
+            {
+                nombre: "King Of Fighters",
+                imagen: "KOF.jpg",
+                audio: "king.mp3"
+            },
+            {
+                nombre: "Sonic",
+                imagen: "sonic.webp",
+                audio: "sonic.mp3"
+            },
+            {
+                nombre: "Super Mario",
+                imagen: "mario.png",
+                audio: "mario.mp3"
+            },
+            {
+                nombre: "Puzzle Bobble",
+                imagen: "Puzzle.png",
+                audio: "Puzzle.mp3"
+            }
+        ];
+    }
